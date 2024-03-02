13bdc47ba0265be7f56517a63059e5052abe554c
Efkey <filipploskonka17@gmail.com>
2024-03-02 22:31:43 +0100

    first stage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,8 +5,6 @@ const Home: React.FC = () => {
   return (
     <div>
       <h1>Homepage</h1>
-      <p>Wejdź do Vocabulary, aby nauczyć się znaczenia słówek po angielsku np "definicja pussy to cat"
-      </p>
       <Link to="/vocabulary">Vocabulary</Link>
       <br />
       <Link to="/progress">Sprawdź progress</Link>
